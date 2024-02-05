ae19412f763488a1addbf5e87736ce20e11bbf2d
VillalobosGarciaGaby <gabriela.garcia.13355797@usalesiana.edu.bo>
2024-02-05 19:00:46 -0400

    space delete

--- src/pages/InventaryPage.jsx
+++ src/pages/InventaryPage.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import NavbarComponent from "../components/NavBarComponent";
 import LateralNav from "../components/lateralNav";
-import { Button, Modal, Box, TextField,Dialog, DialogTitle, DialogContent } from "@mui/material";
+import { Button, Modal, Box, TextField, Dialog, DialogTitle, DialogContent } from "@mui/material";
 import TableProduct from "../components/TableProduct";
 import { getAllProducts, saveDataProduct } from "../services/productService";
 import "../styles/global.css";
@@ -10,18 +10,18 @@ import "../styles/InventaryPage.css";
 const InventaryPage = () => {
     const [products, setProducts] = useState([]);
     const [openModal, setOpenModal] = useState(false);
-    const[nombre, setNombre] = useState("");
-    const[precio, setPrecio] = useState(0);
-    const[cantidad, setCantidad] = useState(0);
-    const[forceUpdate, setForceUpdate] = useState(false);
+    const [nombre, setNombre] = useState("");
+    const [precio, setPrecio] = useState(0);
+    const [cantidad, setCantidad] = useState(0);
+    const [forceUpdate, setForceUpdate] = useState(false);
 
     const fetchData = async () => {
-        try{
+        try {
             const data = await getAllProducts();
             setProducts(data);
             console.log("PRoductos sincronizados");
         }
-        catch(error){
+        catch (error) {
             console.log(error);
         }
     };
@@ -30,7 +30,7 @@ const InventaryPage = () => {
         fetchData();
     }, [forceUpdate]);
 
-    const saveProduct = async() => {
+    const saveProduct = async () => {
         if (precio <= 0 || cantidad < 0) {
             console.error("El precio y la cantidad no pueden ser números negativos");
             return;
@@ -57,40 +57,28 @@ const InventaryPage = () => {
 
     return (
         <div className="page">
-            <NavbarComponent/>
+            <NavbarComponent />
             <div className="body-page">
-                <LateralNav/>
+                <LateralNav />
                 <div className="content-page container">
                     <div className="head-content-tab">
                         <h2>Inventario</h2>
                         <Button onClick={() => setOpenModal(true)} variant="contained" color="primary">Nuevo Producto</Button>
                     </div>
-                    <TableProduct productList={products}/>
+                    <TableProduct productList={products} />
                 </div>
-                <Dialog open={openModal} fullWidth >    
+                <Dialog open={openModal} fullWidth >
                     <DialogTitle> Nuevo Producto </DialogTitle>
                     <DialogContent>
                         <form onSubmit={saveProduct} className="form-product">
-                        <TextField
-  fullWidth
-  variant="standard"
-  label="Nombre"
-  value={nombre}
-  onChange={(e) => {
-    const trimmedValue = e.target.value.slice(0, 50);
-    if (trimmedValue.length <= 50) {
-      setNombre(trimmedValue);
-    }
-  }}
-/>
-
-                            <TextField fullWidth type="number" variant="standard" label="Precio (Bs)" 
+                            <TextField fullWidth variant="standard" label="Nombre" value={nombre} onChange={(e) => {const trimmedValue = e.target.value.slice(0, 50); if (trimmedValue.length <= 50) {setNombre(trimmedValue);}}}/>
+                            <TextField fullWidth type="number" variant="standard" label="Precio (Bs)"
                                 value={precio} onChange={(e) => {
                                     const inputValue = parseFloat(e.target.value);
                                     setPrecio(isNaN(inputValue) ? 0 : Math.max(0, inputValue));
                                 }} />
-                            <TextField fullWidth type="number"  variant="standard" label="Cantidad"
-                                value={cantidad} onChange={(e) => setCantidad(Math.max(0, parseInt(e.target.value)))}    />
+                            <TextField fullWidth type="number" variant="standard" label="Cantidad"
+                                value={cantidad} onChange={(e) => setCantidad(Math.max(0, parseInt(e.target.value)))} />
                             <div className="buttons-form">
                                 <Button variant="outlined" onClick={() => setOpenModal(false)} > Cancelar </Button>
                                 <Button onClick={saveProduct} variant="contained" color="primary">Guardar</Button>
