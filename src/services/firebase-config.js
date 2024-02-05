9c0e9c304d569835d1412fba5a9593e3d5fce648
Mauricio Mamani <wmauchess@gmail.com>
2024-02-05 15:19:32 -0400

    credentials

--- src/services/firebase-config.js
+++ src/services/firebase-config.js
@@ -3,21 +3,13 @@ import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-
   apiKey: "AIzaSyAc8191Qvi6RkPI4sH_N9DgR8vRv-0hzvk",
-
   authDomain: "salesmanager-68efb.firebaseapp.com",
-
   projectId: "salesmanager-68efb",
-
   storageBucket: "salesmanager-68efb.appspot.com",
-
   messagingSenderId: "61865024058",
-
   appId: "1:61865024058:web:95b4bf8202f6fe474bb303",
-
   measurementId: "G-H0GMMWBDDC"
-
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
